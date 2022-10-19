1936385cbdbe4d13831b48cd7f910864b97b9f8f
ahsansiddiqui2 <04611379+ahsansiddiqui2@users.noreply.github.com>
2022-10-19 22:42:11 +0500

    added

---
+++ src/Hotels.py
@@ -0,0 +1,18 @@
+import json 
+    
+
+# python object(dictionary) to be dumped 
+dict1 ={ 
+    "hotel": { 
+        "1": Elem[0], 
+        "2": Elem[1], 
+        "3": Elem[2], 
+        "4": Elem[3], 
+            },  
+} 
+    
+out_file = open("myfile.json", "w") 
+    
+json.dump(dict1, out_file, indent = 6) 
+    
+out_file.close() 
