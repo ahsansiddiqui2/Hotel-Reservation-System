7fa403651a1040141b8b338e08a8e03047d0b9a5
ahsansiddiqui2 <04611379+ahsansiddiqui2@users.noreply.github.com>
2022-10-20 12:17:30 +0500

    added

--- src/Hotels.py
+++ src/Hotels.py
@@ -1,5 +1,12 @@
-import json 
-    
+import json , time, os, getpass
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 Elem = ["phela hotel","dosra hotel","treesa hotel","fourth hotel"]
 # python object(dictionary) to be dumped 
 dict1 ={ 
